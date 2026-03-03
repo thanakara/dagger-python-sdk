29e2eaa450141081b337a0e067d1415c3013d588
thanakara <thkaranikolopoulos@gmail.com>
2026-03-04 00:09:08 +0200

    checkout Caching technique

--- .dagger/src/basics/main.py
+++ .dagger/src/basics/main.py
@@ -42,3 +42,15 @@ class Basics:
             .with_exec(["scripts/dagger-simple"])
             .stdout()
         )
+
+    @function
+    def env(self) -> dagger.Container:
+        """Checkout CACHE technique (try multiple times)"""
+        apt_cache = dag.cache_volume("apt-cache")
+        return (
+            dag.container()
+            .from_("debian:latest")
+            .with_mounted_cache("/var/cache/apt/archives", apt_cache)
+            .with_exec(["apt-get", "update"])
+            .with_exec(["apt-get", "install", "--yes", "mariadb-server", "jq"])
+        )
